1979254b7dd8b85d3adfa546723f2f55dfb5ac88
emal <emalnawabi1@yahoo.com>
2020-12-16 00:09:03 +0430

    build app using next export

---
+++ pages/contact.js
@@ -0,0 +1,33 @@
+export default function contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <form id='contact-form'>
+        <div className='form-group'>
+          <label htmlFor='name'>Name</label>
+          <input type='text' className='form-control' />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='exampleInputEmail1'>Email address</label>
+          <input type='email' className='form-control' />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='message'>Message</label>
+          <textarea className='form-control' rows='5'></textarea>
+        </div>
+        <button type='submit' className='btn btn-primary'>
+          Submit
+        </button>
+      </form>
+
+      <h2>Contact page is aasdlfaksdhfglasdjfga;dfsldfjhgkl;fd</h2>
+      <img src='/' alt='sldfkgjlksdfglkdsl' />
+
+      <p>adslkfghsldkgnlkajdkfhglkskdflgshl;dskafkfgjkshdflkgkds</p>
+      <h3>alkdjglkfasdjfglkdg</h3>
+      <h4>kjsadhljfahldfjald</h4>
+      <h5>a;lkdjf;asdjgl;kjg;fs;d</h5>
+      <h6>hi from h6 or we can say heading 6</h6>
+    </div>
+  )
+}
